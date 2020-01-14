4c0473dda2328f5712cdc4c98f7aa069b2dfc8a5
Shivam Sharma <shivam6279@gmail.com>
2020-01-14 18:47:23 +0530

    g

--- Quadcopter.X/SAM_M8.c
+++
@@ -1,5 +0,0 @@
-#include "GPS.h"
-#include "settings.h"
-#include <math.h>
-#include <xc.h>
-#include <sys/attribs.h>
--- Quadcopter.X/SAM_M8.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SAM_M8_H_
-#define _SAM_M8_H_
-
-#include <stdbool.h>
-
-#endif
